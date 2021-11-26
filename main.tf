3ab079ac81b490b14b28d016d2bee567eac6d063
divvaadarsh <95043495+divvaadarsh@users.noreply.github.com>
2021-11-26 15:29:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 resource "aws_instance" "server2" {
   ami           = "ami-0ba62214afa52bec7"
-  instance_type = "t2.medium"
+  instance_type = "t2.small"
   tags = {
-    Name = "gds-terraform-v2"
+    Name = "ad-terraform-v1"
     Env  = "Prod"
   }
 }
